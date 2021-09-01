5812007aa81666fa1f3c4ade523e2ae4ce1034a4
Vijayakumar D N <89826965+vijayakumardn93@users.noreply.github.com>
2021-09-01 09:27:35 +0530

    Update App.java
    new changes

--- DemoProject/src/main/java/com/abc/App.java
+++ DemoProject/src/main/java/com/abc/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github webhook Hi World!" );
+        System.out.println( "github webhook with ngrok Hi World!" );
     }
 }
